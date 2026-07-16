d03d4489b7680bb8c1a155900c5b655bc3ea059e
Rahmat Hadinata <rsafei731@gmail.com>
2026-07-16 23:39:56 +0700

    feat(edge): initialize flutter desktop project scaffolding

---
+++ android/app/src/main/kotlin/com/lobsense/lobsense_edge_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lobsense.lobsense_edge_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_libserialport
+import media_kit_video
+import package_info_plus
+import sqflite_darwin
+import volume_controller
+import wakelock_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLibserialportPlugin.register(with: registry.registrar(forPlugin: "FlutterLibserialportPlugin"))
+  MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+}
