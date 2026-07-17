8e18524c0567542186c3dc7ec9b579aa97d9415d
Rahmat Hadinata <rsafei731@gmail.com>
2026-07-17 11:02:40 +0700

    feat(edge): wrap MyApp with GatewayStateProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:provider/provider.dart';
+import 'core/services/gateway_state_provider.dart';
 import 'modules/dashboard/pages/dashboard_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   // Inisialisasi MediaKit untuk pemutar video RTSP native
   MediaKit.ensureInitialized();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => GatewayStateProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
